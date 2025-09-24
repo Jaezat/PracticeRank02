0491b70e1e0bf4a9f8fe79f507159076360db68c
Jazmin Acosta <jaezatc@gmail.com>
2025-09-24 20:33:03 +0100

    24/09 20h

--- Practice/fpointer.c
+++ Practice/fpointer.c
@@ -1,29 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list.h                                          :+:      :+:    :+:   */
+/*   fpointer.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mariacos <mariacos@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/24 18:16:46 by mariacos          #+#    #+#             */
-/*   Updated: 2025/09/24 18:34:53 by mariacos         ###   ########.fr       */
+/*   Created: 2025/09/24 18:46:34 by mariacos          #+#    #+#             */
+/*   Updated: 2025/09/24 20:30:44 by mariacos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef FT_LIST_H
-#define FT_LIST_H
 
 #include <stdio.h>
 #include <unistd.h>
 
-typedef struct    s_list
+// functions pointer is basicalluy a pointer, that points to a espace in memory where a function is stored
+
+/* int fun(int a, int b)
 {
-    struct s_list *next;
-    void          *data;
-}                 t_list;
+	return a + b;
+} */
 
 
+void uptolowcase(void *letter)
+{
+	printf("%c", *(char *)letter);
+}
 
+int addtwovoid(void *a, void *b)
+{
+	return (*(int*)a) + (*(int*)b);
+}
 
+int main()
+{
+	int a = 65;
 
-#endif 
+	int b = 99;
+	
+	uptolowcase(&a);
+	printf("%d\n", addtwovoid(&a, &b));
+	
+
+}
